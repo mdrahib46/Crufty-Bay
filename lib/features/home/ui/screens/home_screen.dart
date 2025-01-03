edccc194309261f6ac83916399fe228c1f612a36
mdrahib46 <nahid179846@bsdi-bd.org>
2025-01-03 22:59:21 +0600

    Implemeted Home Screen UI

--- lib/features/home/ui/screens/home_screen.dart
+++ lib/features/home/ui/screens/home_screen.dart
@@ -1,17 +1,14 @@
-// import 'package:carousel_slider/carousel_slider.dart';
-// import 'package:cruftybay/app/app_color.dart';
-//
-// // import 'package:cruftybay/app/app_color.dart';
-import 'package:cruftybay/app/app_color.dart';
 import 'package:cruftybay/app/asset_path.dart';
+import 'package:cruftybay/features/home/ui/widgets/appbar_icon_button.dart';
+import 'package:cruftybay/features/home/ui/widgets/category_item_widget.dart';
+import 'package:cruftybay/features/home/ui/widgets/home_carousel_Slider.dart';
 import 'package:cruftybay/features/home/ui/widgets/home_section_header.dart';
+import 'package:cruftybay/features/home/ui/widgets/product_item_widget.dart';
 import 'package:cruftybay/features/home/ui/widgets/search-bar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
-import '../widgets/appbar_icon_button.dart';
-import '../widgets/category_item_widget.dart';
-import '../widgets/home_carousel_Slider.dart';
+
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -54,60 +51,35 @@ class _HomeScreenState extends State<HomeScreen> {
                 title: "Popular",
                 onTap: () {},
               ),
-              SizedBox(
-                width: 140,
-                child: Card(
-                  color: Colors.white,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(16),
-                  ),
-                  child: Column(
-                    children: [
-                      Container(
-                        decoration: BoxDecoration(
-                            color: AppColors.themeColor.withOpacity(0.14),
-                            borderRadius: const BorderRadius.only(
-                              topLeft: Radius.circular(16),
-                              topRight: Radius.circular(16),
-                            )),
-                        child: Padding(
-                          padding: const EdgeInsets.all(16.0),
-                          child: Image.asset(
-                            'assets/images/product.png',
-                            fit: BoxFit.cover,
-                            width: 120,
-                            height: 90,
-                          ),
-                        ),
-                      ),
-                      const Padding(
-                        padding: EdgeInsets.all(8.0),
-                        child: Column(
-                          children: [
-                            Text(
-                              'HP 15-fd0292TU Core i5 13th Gen',
-                              maxLines: 1,
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                overflow: TextOverflow.ellipsis,
-                              ),
-                            ),
-                            const SizedBox(height: 2),
-                            Row(
-                              children: [
-                                Text(
-                                  '\$100',
-                                  style: TextStyle(
-                                      fontWeight: FontWeight.w600,
-                                      color: AppColors.themeColor),
-                                )
-                              ],
-                            )
-                          ],
-                        ),
-                      )
-                    ],
-                  ),
+              // const ProductItemWidget(),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: _getProductList(),
+                ),
+              ),
+              const SizedBox(height: 8),
+              HomeSectionHeader(
+                title: "Special",
+                onTap: () {},
+              ),
+              // const ProductItemWidget(),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: _getProductList(),
+                ),
+              ),
+              const SizedBox(height: 8),
+              HomeSectionHeader(
+                title: "New",
+                onTap: () {},
+              ),
+              // const ProductItemWidget(),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: _getProductList(),
                 ),
               ),
             ],
@@ -126,6 +98,17 @@ class _HomeScreenState extends State<HomeScreen> {
       ));
     }
     return categoryList;
+  }
+
+  List<Widget> _getProductList(){
+    List<Widget> productList = [];
+    for(int i = 0; i < 10; i++){
+      productList.add(const Padding(
+        padding:  EdgeInsets.only(right: 16.0),
+        child: ProductItemWidget(),
+      ));
+    }
+    return productList;
   }
 
   AppBar _buildAppBar() {
@@ -151,3 +134,4 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 }
+
